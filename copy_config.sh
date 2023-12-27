7b1dd61364a2939b16210aa366d590258266f0bf
emartinez <enri.fc.98@gmail.com>
2023-12-27 19:09:50 +0100

    feat: add bash script to update the repo configuration

---
+++ copy_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NVIM_CONF_PATH="$HOME/.config/nvim/lua/custom/"
+
+cat << EOF
+If you have done changes to nvim configuration, you can save it using this tool.
+Do you want to copy the configuration at $NVIM_CONF_PATH? [y/n]
+EOF
+
+read -rn 1 ans
+echo
+if [[ $ans == "y" ]];
+	then echo "Backing up NvChad configuration at $NVIM_CONF_PATH..."
+		if [[ ! $(test -d "$NVIM_CONF_PATH") ]];
+			then cp -R "$NVIM_CONF_PATH" nvim/lua/
+			echo "Configuration backed-up sucesfully!"
+		fi
+		else
+			echo "I couldn't find any configuration."
+fi
+
